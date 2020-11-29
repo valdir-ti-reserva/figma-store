fbd906840562b2c0ee3dba2c5925e3860c799b6b
valdir-ti <valdir.ti@gmail.com>
2020-11-28 23:13:25 -0300

    feat: create page NotFound

--- src/Routes.js
+++ src/Routes.js
@@ -1,18 +1,22 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 import Products from './pages/Products';
 import ProductDetails from './pages/ProductDetails';
+import NotFound from './pages/NotFound';
 
 const Routes = () => {
   return (
     <>
       <Router>
-        <Route path="/" exact component={Home} />
-        <Route path="/product/:id" exact component={ProductDetails} />
-        <Route path="/products" exact component={Products} />
-        <Route path="/cart" exact component={Cart} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/product/:id" exact component={ProductDetails} />
+          <Route path="/products" exact component={Products} />
+          <Route path="/cart" exact component={Cart} />
+          <Route component={NotFound} />
+        </Switch>
       </Router>
     </>
   )
---
+++ src/pages/NotFound/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from '../../components/Footer';
+import Header from '../../components/Header';
+
+const NotFound = () => {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <h2>Página não encontrada!</h2>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default NotFound
